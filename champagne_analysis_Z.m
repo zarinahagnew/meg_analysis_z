fe9eb29cd8f19704b47e1eafc95d15f3d71e6b3b
Zarinah Agnew <zarinahagnew@gmail.com>
2015-12-21 19:33:41 -0800

    end of day commit

--- champagne_analysis_Z.m
+++ champagne_analysis_Z.m
@@ -1,23 +1,21 @@
-%% for analysis of champagne meg data
+%% For analysis of champagne meg data
 % must already have head model, ROIs and session files -  sess_speak_aligned.mat
-
-%% if have changed the location of the MRI
+% ZKA Dec 2015
+% -------------------------------------------------------------------------
+% NB if have changed the location of the MRI
+% clear all
+% cd /Users/zagnew/EmoVoc_meg/champagne_analysis/
+% load('/Users/zagnew/EmoVoc_meg/champagne_analysis/session_speakaligned.mat')
+% coreg.mripath='/Users/zagnew/EmoVoc_meg/mri/AgnewZarinah_emovoc_V2.img'
+% coreg.norm_mripath='/Users/zagnew/EmoVoc_meg/mri/wAgnewZarinah_emovoc_V2.img'
+% save session_speakaligned 
 clear all
-cd /Users/zagnew/EmoVoc_meg/champagne_analysis/
-load('/Users/zagnew/EmoVoc_meg/champagne_analysis/session_speakaligned.mat')
-coreg.mripath='/Users/zagnew/EmoVoc_meg/mri/AgnewZarinah_emovoc_V2.img'
-coreg.norm_mripath='/Users/zagnew/EmoVoc_meg/mri/wAgnewZarinah_emovoc_V2.img'
-save session_speakaligned 
-clear all
-
-
-
 folder_name = uigetdir
 
 %folder_name = uigetdir(start_path)
 
 cd(folder_name)
-% cd /Users/zagnew/EmoVoc_meg/champagne_analysis/
+% cd /Users/zagnew/EmoVoc_meg/cham_anal/
 
 %% 1. remake l/r session files with 8mm voxel size
 % make new folder with above things in 
@@ -28,6 +26,7 @@ cd(folder_name)
 % save session_speakaligned_rh_voi_laugh.mat
 
 sess_file.sess_file= (sprintf('%s/session_speakaligned.mat',folder_name));
+
 
 gen_nuttf_sessionfile(sess_file.sess_file,'rh',8);
 gen_nuttf_sessionfile(sess_file.sess_file,'lh',8);
@@ -70,8 +69,8 @@ ax = [0 0];
 sess_file.sess_file_rh_laugh=(sprintf('%s/session_speakaligned_rh_voi_laugh.mat',folder_name));
 sess_file.sess_file_lh_laugh=(sprintf('%s/session_speakaligned_lh_voi_laugh.mat',folder_name));
 
-
 cd(folder_name)
+
 
 champagne_wrapper('session_speakaligned_lh_voi_laugh.mat');
 champagne_wrapper('session_speakaligned_rh_voi_laugh.mat');
@@ -86,12 +85,6 @@ champagne_wrapper('session_speakaligned_rh_voi_speech.mat');
 
 
 
-
-
-cd session_speakaligned_lh_voi_laugh_pre-100to-5ms_post25to250ms_vcs2/
-load s_beam_CHAMP2m0_time
-coreg.mripath='/Users/zagnew/EmoVoc_meg/mri/AgnewZarinah_emovoc_V2.img'
-coreg.norm_mripath='/Users/zagnew/EmoVoc_meg/mri/wAgnewZarinah_emovoc_V2.img'
 
 % [sources, ialp, gamma, pow, wts] = ...
 %     run_champagne('/Users/zagnew/EmoVoc_meg/champagne_analysis/cham_anal/','cham_lh_disgust',sess_file.sess_file_lh_laugh,20,t1,t2,t3,t4,init,vcs,ax)
